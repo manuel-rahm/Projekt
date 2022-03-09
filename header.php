9c68efc1be0266cdf78477cf9c3b8c93e036c4f3
manuel-rahm <manuel.rahm@bbz-sh.ch>
2022-03-09 12:39:29 +0100

    1.2

---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="assets/style/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title><?php echo $title ?></title>
+</head>
+
+<body>
+    <nav class="nav" id="navbar">
+        <div class="navTitleContainer"><a class="navTitle" href="index.php">Jünger von Jesus</a></div>
+        <a href="javascript:void(0);" class="burger"><i class="fa fa-bars fa-2x"></i></a>
+        <a class="navLink" href="index.php">Home</a>
+        <a class="navLink" href="galleryoverview.php">Galleries</a>
+        <a class="navLink" href="videos.php">Video Gallery</a>
+        <a class="navLink" href="portraits.php">Portraits</a>
+        <div class="adminContent">
+            <a class="navLink" href="uploadimages.php">Upload Image</a>
+            <a class="navLink" href="uploadvideos.php">Upload Video</a>
+            <a class="navLink" href="usermanagement.php">Manage Users</a>
+        </div>
+        <div class="headerRight">
+            <p class="userInfo">Logged in as: </p>
+            <a class="changePWButton" href="changepw.php">Change PW</a>
+            <form method="POST">
+                <input type="hidden" name="logout">
+                <input class="logoutButton" type="submit" value="Log out">
+            </form>
+        </div>
+    </nav>
---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+$title = "Startseite - JvJ";
+include('header.php');
+?>
+<div id="welcomeScreen">
+    <h1>Welcome to JvJ<br></h1>
+</div>
+<div class="title">
+    <h1>Jünger von Jesus</h1>
+</div>
+<div id="indexContainer" class="container">
+    <div class="row">
+        <div class="column">
+            <h2>Wer sind wir?</h2>
+            <p>Wir sind eine kleine Gruppe von Leuten, die sich meist mit alkoholischen Getränken vergnügen und der grösste Teil spielt gerne Videospiele.</p>
+        </div>
+        <div class="column">
+            <h2>Alter</h2>
+            <p>Wir bestehen aus Jugendlichen und erwachsenen Männern. Die meisten kennen sich seit der Oberstufe.</p>
+        </div>
+        <div class="column">
+            <h2>Musik</h2>
+            <p>Unsere Geschmäcker reichen von Metal bis Hip Hop und EDM, aber wir haben das eine und andere Lied, das jeder von uns feiert.</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="column">
+            <h2>Berufe</h2>
+            <p>Wir decken einen breiten Grad an Berufsgattungen ab. Unsere Berufe sind: Informatiker, Automatiker, Konstrukteure, Mediamatiker, Dachdecker, FaGe, Elektriker, Mechaniker und "Schöggeler".</p>
+        </div>
+        <div class="column">
+            <h2>Orte</h2>
+            <p>Wir alle stammen grundsätzlich aus derselben Region, dem Klettgau. Manche kamen später dazu und andere waren seit Anbeginn im heimeligen Klettgau.</p>
+        </div>
+        <div class="column">
+            <h2>Personen</h2>
+            <p>Wir bestehen momentan aus ca.15 Personen. Die Gruppe kann, bei Bedarf, auch noch wachsen.</p>
+        </div>
+    </div>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="assets/js/functions.js"></script>
+<script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
