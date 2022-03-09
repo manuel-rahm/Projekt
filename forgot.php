1ca9aae741ed3c9fe3738a44883cd114e8d38ced
manuel-rahm <manuel.rahm@bbz-sh.ch>
2022-03-09 10:31:02 +0100

    V1

---
+++ forgot.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="assets/style/loginstyle.css" rel="stylesheet">
+    <title>PW Reset - JvJ</title>
+</head>
+
+<body>
+    <div class="login">
+        <form method="POST">
+            <h1 class="loginTitle">PW Reset - JvJ</h1>
+            <div class="loginBody">
+                <label for="username">Username*:</label>
+                <input class="inputLogin" type="username" name="username" autofocus required>
+                <label for="mail">E-Mail*:</label>
+                <input class="inputLogin" type="mail" name="mail" required>
+                <button class="loginButton" type="submit" value="login">Reset PW</button>
+            </div>
+    </div>
+    </form>
+    </div>
+</body>
+
+</html>
---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="assets/style/loginstyle.css" rel="stylesheet">
+    <title>Login - JvJ</title>
+</head>
+
+<body>
+    <div class="login">
+        <h1 class="loginTitle">Login - JvJ</h1>
+        <form method="POST">
+            <div class="loginBody">
+                <label for="username">Username*:</label>
+                <input class="inputLogin" type="username" name="username" autofocus required>
+                <label for="password">Password*:</label>
+                <input class="inputLogin" type="password" name="password" required>
+                <button class="loginButton" type="submit" value="Submit" name="submit">Log In</button>
+                <a class="forgot" href="forgot.php">Forgot your password?</a>
+            </div>
+    </div>
+    </form>
+    </div>
+</body>
+
+</html>
