a4acb7f78cd2ce399d5202e6276c83db1090ac54
manuel-rahm <manuel.rahm@bbz-sh.ch>
2022-03-10 15:42:28 +0100

    1.9

--- galleryoverview.php
+++ galleryoverview.php
@@ -7,77 +7,77 @@ include('header.php');
     <div class="row">
         <div class="column">
             <h2 class="overviewTitle">Andri</h2>
-            <a href="gallery.php?gallery=Andri"><img class="overviewImg" src="upload/Images/Overview/"></a>
+            <a href="gallery.php?gallery=Andri"><img class="overviewImg" src="upload/Images/Overview/Aatrox.jpg"></a>
         </div>
         <div class="column">
             <h2 class="overviewTitle">Andrin</h2>
-            <a href="gallery.php?gallery=Andrin"><img class="overviewImg" src="upload/Images/Overview/"></a>
+            <a href="gallery.php?gallery=Andrin"><img class="overviewImg" src="upload/Images/Overview/Brand.jpg"></a>
         </div>
         <div class="column">
             <h2 class="overviewTitle">Brian</h2>
-            <a href="gallery.php?gallery=Brian"><img class="overviewImg" src="upload/Images/Overview/"></a>
+            <a href="gallery.php?gallery=Brian"><img class="overviewImg" src="upload/Images/Overview/Darius.jpg"></a>
         </div>
     </div>
     <div class="row">
         <div class="column">
             <h2 class="overviewTitle">Jan</h2>
-            <a href="gallery.php?gallery=Jan"><img class="overviewImg" src="upload/Images/Overview/"></a>
+            <a href="gallery.php?gallery=Jan"><img class="overviewImg" src="upload/Images/Overview/Fizz.jpg"></a>
         </div>
         <div class="column">
             <h2 class="overviewTitle">Joël</h2>
-            <a href="gallery.php?gallery=Joël"><img class="overviewImg" src="upload/Images/Overview/"></a>
+            <a href="gallery.php?gallery=Joël"><img class="overviewImg" src="upload/Images/Overview/Irelia.jpg"></a>
         </div>
         <div class="column">
             <h2 class="overviewTitle">Julian</h2>
-            <a href="gallery.php?gallery=Julian"><img class="overviewImg" src="upload/Images/Overview/"></a>
+            <a href="gallery.php?gallery=Julian"><img class="overviewImg" src="upload/Images/Overview/Jhin.jpg"></a>
         </div>
     </div>
     <div class="row">
         <div class="column">
             <h2 class="overviewTitle">Kilian</h2>
-            <a href="gallery.php?gallery=Kilian"><img class="overviewImg" src="upload/Images/Overview/"></a>
+            <a href="gallery.php?gallery=Kilian"><img class="overviewImg" src="upload/Images/Overview/Kaisa.jpg"></a>
         </div>
         <div class="column">
             <h2 class="overviewTitle">Manuel</h2>
-            <a href="gallery.php?gallery=Manuel"><img class="overviewImg" src="upload/Images/Overview/"></a>
+            <a href="gallery.php?gallery=Manuel"><img class="overviewImg" src="upload/Images/Overview/Karma.jpg"></a>
         </div>
         <div class="column">
             <h2 class="overviewTitle">Mathis</h2>
-            <a href="gallery.php?gallery=Mathis"><img class="overviewImg" src="upload/Images/Overview/"></a>
+            <a href="gallery.php?gallery=Mathis"><img class="overviewImg" src="upload/Images/Overview/Sion.jpg"></a>
         </div>
     </div>
     <div class="row">
         <div class="column">
             <h2 class="overviewTitle">Noah</h2>
-            <a href="gallery.php?gallery=Noah"><img class="overviewImg" src="upload/Images/Overview/"></a>
+            <a href="gallery.php?gallery=Noah"><img class="overviewImg" src="upload/Images/Overview/teemo.jpg"></a>
         </div>
         <div class="column">
             <h2 class="overviewTitle">Pascal</h2>
-            <a href="gallery.php?gallery=Pascal"><img class="overviewImg" src="upload/Images/Overview/"></a>
+            <a href="gallery.php?gallery=Pascal"><img class="overviewImg" src="upload/Images/Overview/Vex.jpg"></a>
         </div>
         <div class="column">
             <h2 class="overviewTitle">Sidney</h2>
-            <a href="gallery.php?gallery=Sidney"><img class="overviewImg" src="upload/Images/Overview/"></a>
+            <a href="gallery.php?gallery=Sidney"><img class="overviewImg" src="upload/Images/Overview/Vi.jpg"></a>
         </div>
     </div>
     <div class="row">
         <div class="column">
             <h2 class="overviewTitle">Simon</h2>
-            <a href="gallery.php?gallery=Simon"><img class="overviewImg" src="upload/Images/Overview/"></a>
+            <a href="gallery.php?gallery=Simon"><img class="overviewImg" src="upload/Images/Overview/Yasuo.png"></a>
         </div>
         <div class="column">
             <h2 class="overviewTitle">Vitus</h2>
-            <a href="gallery.php?gallery=Vitus"><img class="overviewImg" src="upload/Images/Overview/"></a>
+            <a href="gallery.php?gallery=Vitus"><img class="overviewImg" src="upload/Images/Overview/Zed.jpg"></a>
         </div>
         <div class="column">
             <h2 class="overviewTitle">Yanik</h2>
-            <a href="gallery.php?gallery=Yanik"><img class="overviewImg" src="upload/Images/Overview/"></a>
+            <a href="gallery.php?gallery=Yanik"><img class="overviewImg" src="upload/Images/Overview/Zeri.jpg"></a>
         </div>
     </div>
     <div class="row">
         <div class="column">
             <h2 class="overviewTitle">Group</h2>
-            <a href="gallery.php?gallery=Group"><img class="overviewImg" src="upload/Images/Overview/"></a>
+            <a href="gallery.php?gallery=Group"><img class="overviewImg" src="upload/Images/Overview/group.jpg"></a>
         </div>
     </div>
 </div>
